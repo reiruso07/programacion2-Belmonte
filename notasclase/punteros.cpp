44dd076ccd6d8ced59d5a352aff74633485e1efb
Reinaldo Belmonte <reinaldobelmonte9@gmail.com>
2026-01-30 15:10:25 -0400

    feat añadiendo mas cosas a las notas de clase

--- notasclase/punteros.cpp
+++ notasclase/punteros.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 using namespace std;
+void modificarvalorporpuntero(int *ola){
+    *ola+=1;
+}
 int main(){
     int numero=42,arreglo[]={1,2,3,4,5};
     int *ptr=&numero,*ptrarr=arreglo;
@@ -7,18 +10,22 @@ int main(){
     cout<<"Valor del puntero: "<<*ptr<<endl;
     cout<<"direccion del puntero: "<<ptr<<endl;
     cout<<"direccion del valor: "<<&numero<<endl;
-   cout<<"Tamaño del puntero: "<<sizeof(ptr);
+    int *arreglodinamico= new int[5];
     for(int i=0;i<5;i++){
         cout<<"puntero que apunta al arreglo: "<<*ptrarr+i<<endl;
         cout<<"EL arreglo: "<<arreglo[i]<<endl;
         cout<<"dirreccion del arreglo que tiene el puntero que apunta al arreglo: "<<ptrarr+i<<endl;
         cout<<"direccion de EL arreglo: "<<&arreglo[i]<<endl;
     }
+    delete[] arreglodinamico;
+    arreglodinamico=nullptr;
     int* punterodinamico= new int;
     *punterodinamico=500;// o int* punterodinamico= new int(500);
     cout<<"El valor del puntero dinamico: "<<*punterodinamico<<endl;
     delete punterodinamico;
     punterodinamico=nullptr;
     cout<<"El valor del puntero dinamico: "<<punterodinamico<<endl; //no se puede imprimir debido a que su valo es null
+    modificarvalorporpuntero(ptr);
+    cout<<"El valor se cambio gracias a la funcion modificarvalorporpuntero: "<<numero<<endl;
     return 0;
 }
